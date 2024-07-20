cd42aa114e6a3caeababa4672ac71c2640e20831
gutivanian <gutivanaliefs@gmail.com>
2024-07-20 23:00:03 +0700

    a

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/NavigationBar.css';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
